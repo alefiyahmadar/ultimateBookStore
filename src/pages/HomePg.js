e2c804c34dc96fea0baa2d5a06cff40e09a03c88
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2024-08-24 11:07:14 +0000

    CSS

--- src/pages/HomePg.js
+++ src/pages/HomePg.js
@@ -15,12 +15,16 @@ export const HomePage = () => {
   };
   return (
     <div>
-      {category.map(({ categoryName, description, _id }) => (
+      <div className="homeImgContainer">
+        <img src="https://plus.unsplash.com/premium_photo-1669323925724-bb87080d122d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3N0b3JlJTIwY2FydG9vbnxlbnwwfHwwfHx8MA%3D%3D"></img>
+      </div>
+      {category.map(({ categoryName, description, _id, image }) => (
         <div
           className="homePgContainer"
           id={_id}
           style={{ display: "inline-block", padding: "1rem" }}
         >
+          <img style={{}} src={image}></img>
           <h3 onClick={() => GetCategory(categoryName)}>{categoryName}</h3>
         </div>
       ))}
