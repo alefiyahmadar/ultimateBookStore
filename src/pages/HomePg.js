1807557c9e51547e2a8c7df8556a3d084b04061d
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2024-08-24 10:24:49 +0000

    initial-commit

--- src/pages/HomePg.js
+++ src/pages/HomePg.js
@@ -3,13 +3,25 @@ import { AppContext } from "../contextProvider";
 import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
-  const { category, setCategory } = useContext(AppContext);
+  const { category, setCategory, filters, setFilter } = useContext(AppContext);
   const navigate = useNavigate();
+  const GetCategory = (categoryName) => {
+    console.log(categoryName);
+    setFilter({
+      ...filters,
+      categoryValue: [...filters.categoryValue, categoryName],
+    });
+    navigate("/product");
+  };
   return (
     <div>
       {category.map(({ categoryName, description, _id }) => (
-        <div id={_id} style={{ display: "inline-block", padding: "1rem" }}>
-          <h3>{categoryName}</h3>
+        <div
+          className="homePgContainer"
+          id={_id}
+          style={{ display: "inline-block", padding: "1rem" }}
+        >
+          <h3 onClick={() => GetCategory(categoryName)}>{categoryName}</h3>
         </div>
       ))}
 
