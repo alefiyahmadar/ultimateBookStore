45127de0d3004056693479920ad22031aac3df9e
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2024-08-27 10:50:31 +0000

    styling-detailCard

--- src/pages/productDetailPg.js
+++ src/pages/productDetailPg.js
@@ -5,7 +5,14 @@ import { AppContext } from "../contextProvider";
 export const PoductDetail = () => {
   const { prodId } = useParams();
 
-  const { product, setProduct } = useContext(AppContext);
+  const {
+    product,
+    setProduct,
+    AddToCartBtn,
+    RemoveCartBtn,
+    AddToWishlistBtn,
+    RemoveWishBtn,
+  } = useContext(AppContext);
 
   const getProduct = product.find((e) => e._id === prodId);
   console.log(getProduct);
@@ -19,12 +26,64 @@ export const PoductDetail = () => {
           </div>
           <div style={{}}>
             <span className="detailBar">
-              <p>{getProduct.title}</p>
-              <p>{getProduct.rating}⭐</p>
-              <p>{getProduct.author}</p>
-              <p>{getProduct.nonfiction}</p>
-              <p>{getProduct.fiction}</p>
-              <p>{getProduct.horror}</p>
+              <h3>{getProduct.title}</h3>
+              <h4>{getProduct.author}</h4>
+              <h5 className="ratingD">{getProduct.rating}⭐</h5>
+              <h5>₹{getProduct.price}</h5>
+              <p style={{ display: "flex", color: "gray" }}>
+                <span style={{ fontSize: "10px" }} role="img" aria-label="">
+                  ⚡
+                </span>
+                Hurry, Only few left!
+              </p>
+              <hr />
+              <p>
+                Author: <span>{getProduct.author}</span>
+              </p>
+              <p>
+                Category:{" "}
+                <span>
+                  {getProduct.fiction}
+                  {getProduct.nonfiction}
+                  {getProduct.horror}
+                </span>
+              </p>
+              <p>
+                {" "}
+                Language: <span>{getProduct.language}</span>
+              </p>
+              <button
+                className="CartbtnDetail"
+                onClick={
+                  JSON.parse(localStorage.getItem("user")).cart.find(
+                    (e) => e.title === getProduct.title
+                  )
+                    ? () => RemoveCartBtn(getProduct)
+                    : () => AddToCartBtn(getProduct)
+                }
+              >
+                {JSON.parse(localStorage.getItem("user")).cart.find(
+                  (e) => e.title === getProduct.title
+                )
+                  ? "Remove From Cart"
+                  : "Add To Cart"}
+              </button>
+              <button
+                className="WishbtnDetail"
+                onClick={
+                  JSON.parse(localStorage.getItem("user")).wishlist.find(
+                    (e) => e.title === getProduct.title
+                  )
+                    ? () => RemoveWishBtn(getProduct)
+                    : () => AddToWishlistBtn(getProduct)
+                }
+              >
+                {JSON.parse(localStorage.getItem("user")).wishlist.find(
+                  (e) => e.title === getProduct.title
+                )
+                  ? "Remove From Wishlist"
+                  : "Add To Wishlist"}
+              </button>
             </span>
           </div>
         </span>
